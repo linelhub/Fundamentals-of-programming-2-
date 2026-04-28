cc321af5bde54f16b047322a4c6115a72fc09283
linelhub <guentalinel@gmail.com>
2026-04-28 22:49:33 +0100

    Add files via upload
    Writing a C++ program that uses functions and loops to display a diamond shape made of asterisks (*) on the screen

---
+++ Diamond.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+void Diamond(int n){
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n-i; j++){
+            cout<< " ";
+        }
+        for(int k=1; k<=2*i-1; k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i=n-1; i>=1; i--){
+        for (int j=1; j<=n-i; j++){
+            cout<< " ";
+        }
+        for (int k=1; k<=2*i-1; k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the Diamond:";
+    cin>>n;
+
+    Diamond(n);
+    return 0;
+
+}
+
